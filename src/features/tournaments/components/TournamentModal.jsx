32187e1ba2afd30b4aa97e721222bfbd4d126cd7
Breyner Benitez <bbenitez-2021497@kinal.edu.gt>
2026-04-20 11:51:42 -0600

    Implementación de variables de entorno

--- src/features/tournaments/components/TournamentModal.jsx
+++ src/features/tournaments/components/TournamentModal.jsx
@@ -1,6 +1,4 @@
-export const TournamentModal = ({ isOpen }) => {
-    
-
+export const TournamentModal = () => {
     return (
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 px-3 sm:px-4">
             <div className="bg-white rounded-2xl shadow-2xl w-full max-w-lg md:max-w-2xl max-h-[90vh] flex flex-col overflow-hidden">
